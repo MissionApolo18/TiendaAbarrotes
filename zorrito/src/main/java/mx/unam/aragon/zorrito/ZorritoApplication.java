10d50580ac49e512d2a7b9b3c76a2c7985af3948
VazRos03 <luisyuyots@gmail.com>
2025-05-05 09:50:47 -0600

    Zorrito

---
+++ zorrito/src/main/java/mx/unam/aragon/zorrito/ZorritoApplication.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.zorrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZorritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZorritoApplication.class, args);
+	}
+
+}
---
+++ zorrito/src/test/java/mx/unam/aragon/zorrito/ZorritoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.zorrito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZorritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
